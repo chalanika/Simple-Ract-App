f94201327c781fd5347448cd229c12adf8b75d74
Chalanika <chalanikasewwandi@gmail.com>
2020-05-05 22:18:28 +0530

    create badge

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {}
+    state = {
+        count:0,
+        imageUrl:"https://image.shutterstock.com/image-photo/mountains-during-sunset-beautiful-natural-260nw-407021107.jpg"
+    }
+
+    style ={
+        fontSize:20,
+        fontWeight:"bold"
+    }
     render() {
+        
         return (
-            <React.Fragment>
-                <h1>Hello World</h1>
-                <button>sent</button>
-            </React.Fragment>
+            <div>
+                <span  className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button className="btn btn-success m-3">sent</button>
+            </div>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge badge-";
+        classes += this.state.count === 0 ? "primary" : "warning";
+        return classes;
+    }
+
+    formatCount(){
+        const {count} = this.state;
+        return count === 0? "zero" : count;
     }
 }
 
